631cfcd9e6d976a76cafd494730b7892fe7da4c7
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-12-11 11:49:41 +0100

    Add project files.

---
+++ BarlangCLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using BarlangCLI;
+using System.Text;
+
+const string FILE = "..\\..\\..\\resources\\barlangok.txt";
+List<Barlang> barlangok = [];
+
+using StreamReader sr = new(FILE, Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) barlangok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"4. feladat: barlangok szama: {barlangok.Count}");
